507fc5446b47aaf503b07659270ba7dddeff92ac
Repesco Maxim <maximrepesco@gmail.com>
2024-03-07 02:17:05 +0200

    final commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ca9e0b8312msh07cc5a481801adep13695cjsn4d29cff25437',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6c3da40538b6781159028c20aee534d4";
